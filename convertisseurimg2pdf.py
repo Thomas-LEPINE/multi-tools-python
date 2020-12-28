a2b1a4ca4e854e06af4530c108342a70191f4810
Thomas-LEPINE <64126235+Thomas-LEPINE@users.noreply.github.com>
2020-12-28 12:43:34 +0100

    +

--- convertisseurimg2pdf.py
+++ convertisseurimg2pdf.py
@@ -2,13 +2,10 @@
 #  Copyright : Thomas LÉPINE  (thomas.lep4@gmail.com)
 # ------------------------------------------------------
 
-# import tkinter as tk
-# from tkinter import ttk
 from tkinter import filedialog
 from tkinter import messagebox
 from tkinter import *
 from PIL import Image
-from PIL import ImageTk
 
 imageList = []
 
@@ -34,13 +31,31 @@ def exit():
     if msgBox == 'yes':
        window.destroy()
 
-# Options de la fenêtre
-windowOptions = {'width':850, 'height':500, 'background':"#004875"}
+''' HELP : '''
+def help():
+    messagebox.showinfo(title="Aide", icon = 'question' , message="Ce programme permet de convertir des images en un fichier pdf.\n\n1 : Chargez la ou les images souhaitées à l'aide du boutton \"Fichier(s)\".\n\n2 : Cliquez sur \"Convertir en PDF\" et selectionnez le dossier dans lequel vous souhaitez enregistrer le PDF.\n\nEt voilà ! Vous pouvez ensuite quitter l'application (n'hésitez pas à aller vérifier que la convertion a bien fontionné !)\n\n\nProgramme développé par Thomas Lépine (thomas.lep4@gmail.com)")
+
+# Options de la fenêtre :
+windowOptions = {'width':900, 'height':550, 'background':"#996B6B"} # Bleu foncé : #004875
+
+# Options des bouttons :
+butonsOptions = {
+    'policeButons':"Ebrima",
+    'sizeButons':15,
+    'textButon1':"        Fichier(s)        ",
+    'colorButon1':"#5C8199",
+    'textButon2':"  Convertir en PDF  ",
+    'colorButon2':"#6B9978",
+    'textButonHelp':"   Aide   ",
+    'colorButonHelp':"#996399",
+    'textButonExit':"   Quitter   ",
+    'colorButonExit':"#E55C5C",
+    }
 
 # SETUP : ---------------------------------------------
 window = Tk()
 window.title('Tool : Image to PDF')
-window.iconbitmap("./assets/logo_alligator_news_ico.ico")
+window.iconbitmap("./assets/logo_multi_tools.ico")
 window.config(background=windowOptions['background'])
 window.geometry(str(windowOptions['width']) + "x" + str(windowOptions['height']))
 window.minsize(800, 500)
@@ -52,48 +67,45 @@ canvas = Frame(window, background = windowOptions['background'])
 
 ########### HEADER
 headerFrame = Frame(canvas, background = windowOptions['background'])
-width = 200
-height = 100
+width = 220
+height = 210
 #Image ---------------------------------------------
-imageFrame = Frame(headerFrame, background = windowOptions['background'])
-image = PhotoImage(file="./assets/alli.png").zoom(2).subsample(10)
-imageCanvas = Canvas(imageFrame, width=width, height=height, background = windowOptions['background'], border=0, highlightthickness=0)
+image = PhotoImage(file="./assets/logo_multi_tools.png").zoom(2).subsample(17)
+imageCanvas = Canvas(headerFrame, width=width, height=height, background = windowOptions['background'], border=0, highlightthickness=0)
 imageCanvas.create_image(width/2, height/2, image=image)
-imageCanvas.pack()
-imageFrame.grid(row=0, column=0, sticky=N)
+imageCanvas.grid(row=0, column=0, sticky=W)
 ####### --------------------------------------------
 #Titre ---------------------------------------------
-titreFrame = Frame(headerFrame, background = windowOptions['background'])
-titre = Label(titreFrame, text='Outil de convertion\n~ Image(s) -> PDF ~', background='#C8C8C8', font=('Ink Free', 30, 'bold'), fg='#000')
-titre.pack()
-titreFrame.grid(row=0, column=1, sticky=N)
+titre = Label(headerFrame, text=' Outil de convertion \n  ~ Image(s) -> PDF ~  ', background = windowOptions['background'], font=('Ink Free', 30, 'bold'), fg='#000',  border=2, relief=SUNKEN)
+titre.grid(row=0, column=1, sticky=W)
 ####### --------------------------------------------
 #Image ---------------------------------------------
-imageFrame2 = Frame(headerFrame, background = windowOptions['background'])
-image2 = PhotoImage(file="./assets/alli.png").zoom(2).subsample(10)
-imageCanvas2 = Canvas(imageFrame2, width=width, height=height, background = windowOptions['background'], border=0, highlightthickness=0)
+image2 = PhotoImage(file="./assets/logo_img2pdf.png").zoom(2).subsample(17)
+imageCanvas2 = Canvas(headerFrame, width=width, height=height, background = windowOptions['background'], border=0, highlightthickness=0)
 imageCanvas2.create_image(width/2, height/2, image=image2)
-imageCanvas2.pack()
-imageFrame2.grid(row=0, column=2, sticky=N)
+imageCanvas2.grid(row=0, column=2, sticky=W)
 ####### ---------------------------------------------
-headerFrame.grid(row=0, column=0, sticky=N)
+headerFrame.grid(row=0, column=0, sticky=W)
 ############## ---------------------------------------------
 
 #Boutons ---------------------------------------------
 butonsFrame = Frame(canvas, background = windowOptions['background'])
-
-getFileButton = Button(butonsFrame, text='        Fichier(s)        ', command=getFiles, background='#E8927C', fg='black', font=('Ebrima', 14, 'bold'))
+# ---
+getFileButton = Button(butonsFrame, text=butonsOptions['textButon1'], command=getFiles, background=butonsOptions['colorButon1'], font=(butonsOptions['policeButons'], butonsOptions['sizeButons'], 'bold'), fg='black')
 getFileButton.pack(pady=windowOptions['height']/20)
-convertButton = Button(butonsFrame, text='   Convertion en PDF   ', command=convertToPdf, background='#0B8000', fg='black', font=('Ebrima', 14, 'bold'))
+convertButton = Button(butonsFrame, text=butonsOptions['textButon2'], command=convertToPdf, background=butonsOptions['colorButon2'], fg='black', font=(butonsOptions['policeButons'], butonsOptions['sizeButons'], 'bold'))
 convertButton.pack(pady=windowOptions['height']/80)
-
+# ---
 bottomButonsFrame = Frame(butonsFrame, background = windowOptions['background'])
-helpButton = Button (bottomButonsFrame, text='   AIDE   ', command=exit, background='#CD010D', fg='white', font=('Ebrima', 16, 'bold'))
+helpButton = Button (bottomButonsFrame, text=butonsOptions['textButonHelp'], command=help, background=butonsOptions['colorButonHelp'], fg='black', font=(butonsOptions['policeButons'], butonsOptions['sizeButons'], 'bold'))
 helpButton.grid(row=0, column=0, sticky=W)
 
-exitButton = Button (bottomButonsFrame, text='   Quitter   ', command=exit, background='#CD010D', fg='white', font=('Ebrima', 16, 'bold'))
-exitButton.grid(row=0, column=2, sticky=E)
-bottomButonsFrame.pack(pady=windowOptions['height']/20)
+espace = Label(bottomButonsFrame, text='                       ', background = windowOptions['background'])
+espace.grid(row=0, column=1, sticky=W)
+
+exitButton = Button (bottomButonsFrame, text=butonsOptions['textButonExit'], command=exit, background=butonsOptions['colorButonExit'], fg='black', font=(butonsOptions['policeButons'], butonsOptions['sizeButons'], 'bold'))
+exitButton.grid(row=0, column=2, sticky=W)
+bottomButonsFrame.pack(pady=windowOptions['height']/15)
 
 butonsFrame.grid(row=2, column=0, sticky=S)
 ####### ---------------------------------------------
